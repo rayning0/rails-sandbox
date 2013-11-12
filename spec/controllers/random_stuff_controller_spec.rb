c1f0b5b6e7b5a983a35561ddebf72e2a7a9947db
Spencer Rogers <spencer1248@gmail.com>
2013-11-12 08:39:11 -0500

    Add tests for status codes and rendering nothing

--- spec/controllers/random_stuff_controller_spec.rb
+++ spec/controllers/random_stuff_controller_spec.rb
@@ -10,8 +10,25 @@ describe RandomStuffController do
   end
 
   describe "GET nothing" do
-    get "nothing"
 
-    response.body.should == ""
+    it "returns nothing" do
+      get "nothing"
+
+      response.body.should == ""
+    end
+  end
+
+  describe "get classified page" do
+    it "should return a 401 without a token"
+      get :classified
+
+      assert_response(401)
+    end
+
+    it "should return a 200 with a token"
+      get :classified, token: '123456'
+
+      assert_response(200)
+    end
   end
 end
